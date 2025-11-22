69120b10327fd47ed0594658ed5e9534048adcae
김형준 <>
2025-11-22 20:00:25 +0900

    bad-*테스트 통과 코드

--- pintos/userprog/exception.c
+++ pintos/userprog/exception.c
@@ -7,10 +7,11 @@
 #include "intrinsic.h"
 
 /* Number of page faults processed. */
+// 디버깅용으로 페이지 폴트가 몇 번 일어났는지 셉니다.
 static long long page_fault_cnt;
 
-static void kill (struct intr_frame *);
-static void page_fault (struct intr_frame *);
+static void kill(struct intr_frame *);
+static void page_fault(struct intr_frame *);
 
 /* Registers handlers for interrupts that can be caused by user
    programs.
@@ -27,48 +28,55 @@ static void page_fault (struct intr_frame *);
 
    Refer to [IA32-v3a] section 5.15 "Exception and Interrupt
    Reference" for a description of each of these exceptions. */
-void
-exception_init (void) {
+
+// (NULL 포인터, 커널 영역, 할당받지 않은 영역 등)에 접근했을 때 OS가 이를 감지하고 프로세스를 올바르게 종료
+//	핀토스 부팅 시(init.c) 한 번 호출
+//	CPU의 **IDT(Interrupt Descriptor Table)**에 "이런 에러가 나면 이 함수를 실행해!"
+void exception_init(void)
+{
 	/* These exceptions can be raised explicitly by a user program,
 	   e.g. via the INT, INT3, INTO, and BOUND instructions.  Thus,
 	   we set DPL==3, meaning that user programs are allowed to
 	   invoke them via these instructions. */
-	intr_register_int (3, 3, INTR_ON, kill, "#BP Breakpoint Exception");
-	intr_register_int (4, 3, INTR_ON, kill, "#OF Overflow Exception");
-	intr_register_int (5, 3, INTR_ON, kill,
-			"#BR BOUND Range Exceeded Exception");
+	intr_register_int(3, 3, INTR_ON, kill, "#BP Breakpoint Exception");
+	intr_register_int(4, 3, INTR_ON, kill, "#OF Overflow Exception");
+	intr_register_int(5, 3, INTR_ON, kill,
+					  "#BR BOUND Range Exceeded Exception");
 
 	/* These exceptions have DPL==0, preventing user processes from
 	   invoking them via the INT instruction.  They can still be
 	   caused indirectly, e.g. #DE can be caused by dividing by
 	   0.  */
-	intr_register_int (0, 0, INTR_ON, kill, "#DE Divide Error");
-	intr_register_int (1, 0, INTR_ON, kill, "#DB Debug Exception");
-	intr_register_int (6, 0, INTR_ON, kill, "#UD Invalid Opcode Exception");
-	intr_register_int (7, 0, INTR_ON, kill,
-			"#NM Device Not Available Exception");
-	intr_register_int (11, 0, INTR_ON, kill, "#NP Segment Not Present");
-	intr_register_int (12, 0, INTR_ON, kill, "#SS Stack Fault Exception");
-	intr_register_int (13, 0, INTR_ON, kill, "#GP General Protection Exception");
-	intr_register_int (16, 0, INTR_ON, kill, "#MF x87 FPU Floating-Point Error");
-	intr_register_int (19, 0, INTR_ON, kill,
-			"#XF SIMD Floating-Point Exception");
+	intr_register_int(0, 0, INTR_ON, kill, "#DE Divide Error");
+	intr_register_int(1, 0, INTR_ON, kill, "#DB Debug Exception");
+	intr_register_int(6, 0, INTR_ON, kill, "#UD Invalid Opcode Exception");
+	intr_register_int(7, 0, INTR_ON, kill,
+					  "#NM Device Not Available Exception");
+	intr_register_int(11, 0, INTR_ON, kill, "#NP Segment Not Present");
+	intr_register_int(12, 0, INTR_ON, kill, "#SS Stack Fault Exception");
+	intr_register_int(13, 0, INTR_ON, kill, "#GP General Protection Exception");
+	intr_register_int(16, 0, INTR_ON, kill, "#MF x87 FPU Floating-Point Error");
+	intr_register_int(19, 0, INTR_ON, kill,
+					  "#XF SIMD Floating-Point Exception");
 
 	/* Most exceptions can be handled with interrupts turned on.
 	   We need to disable interrupts for page faults because the
 	   fault address is stored in CR2 and needs to be preserved. */
-	intr_register_int (14, 0, INTR_OFF, page_fault, "#PF Page-Fault Exception");
+	//**14번(Page Fault)**만 page_fault 함수로 연결
+	/* CR2 레지스터 보존을 위해 인터럽트를 끄고(INTR_OFF) 진입 */
+	intr_register_int(14, 0, INTR_OFF, page_fault, "#PF Page-Fault Exception");
 }
 
 /* Prints exception statistics. */
-void
-exception_print_stats (void) {
-	printf ("Exception: %lld page faults\n", page_fault_cnt);
+void exception_print_stats(void)
+{
+	printf("Exception: %lld page faults\n", page_fault_cnt);
 }
 
 /* Handler for an exception (probably) caused by a user process. */
 static void
-kill (struct intr_frame *f) {
+kill(struct intr_frame *f)
+{
 	/* This interrupt is one (probably) caused by a user process.
 	   For example, the process might have tried to access unmapped
 	   virtual memory (a page fault).  For now, we simply kill the
@@ -79,33 +87,39 @@ kill (struct intr_frame *f) {
 
 	/* The interrupt frame's code segment value tells us where the
 	   exception originated. */
-	switch (f->cs) {
-		case SEL_UCSEG:
-			/* User's code segment, so it's a user exception, as we
-			   expected.  Kill the user process.  */
-			printf ("%s: dying due to interrupt %#04llx (%s).\n",
-					thread_name (), f->vec_no, intr_name (f->vec_no));
-			intr_dump_frame (f);
-			thread_exit ();
 
-		case SEL_KCSEG:
-			/* Kernel's code segment, which indicates a kernel bug.
-			   Kernel code shouldn't throw exceptions.  (Page faults
-			   may cause kernel exceptions--but they shouldn't arrive
-			   here.)  Panic the kernel to make the point.  */
-			intr_dump_frame (f);
-			PANIC ("Kernel bug - unexpected interrupt in kernel");
+	/* f->cs (Code Segment)를 보면 누가 에러를 냈는지 알 수 있음 */
+	switch (f->cs)
+	{
+		//(User Mode에서 에러 발생)
+	case SEL_UCSEG:
+		/* User's code segment, so it's a user exception, as we
+		   expected.  Kill the user process.  */
+		printf("%s: dying due to interrupt %#04llx (%s).\n",
+			   thread_name(), f->vec_no, intr_name(f->vec_no));
+		intr_dump_frame(f);
+		thread_exit();
 
-		default:
-			/* Some other code segment?  Shouldn't happen.  Panic the
-			   kernel. */
-			printf ("Interrupt %#04llx (%s) in unknown segment %04x\n",
-					f->vec_no, intr_name (f->vec_no), f->cs);
-			thread_exit ();
+	// 커널 코드 세그먼트 (Kernel Mode에서 에러 발생)
+	case SEL_KCSEG:
+		/* Kernel's code segment, which indicates a kernel bug.
+		   Kernel code shouldn't throw exceptions.  (Page faults
+		   may cause kernel exceptions--but they shouldn't arrive
+		   here.)  Panic the kernel to make the point.  */
+		intr_dump_frame(f);
+		// OS 전체 정지 (Kernel Panic)
+		PANIC("Kernel bug - unexpected interrupt in kernel");
+
+	default:
+		/* Some other code segment?  Shouldn't happen.  Panic the
+		   kernel. */
+		printf("Interrupt %#04llx (%s) in unknown segment %04x\n",
+			   f->vec_no, intr_name(f->vec_no), f->cs);
+		thread_exit();
 	}
 }
 
-/* Page fault handler.  This is a skeleton that must be filled in
+/* Page fault handler.  This is a skeleton  that must be filled in
    to implement virtual memory.  Some solutions to project 2 may
    also require modifying this code.
 
@@ -116,33 +130,43 @@ kill (struct intr_frame *f) {
    can find more information about both of these in the
    description of "Interrupt 14--Page Fault Exception (#PF)" in
    [IA32-v3a] section 5.15 "Exception and Interrupt Reference". */
+
+// page_fault(struct intr_frame *f): 페이지 폴트 처리
 static void
-page_fault (struct intr_frame *f) {
-	bool not_present;  /* True: not-present page, false: writing r/o page. */
-	bool write;        /* True: access was write, false: access was read. */
-	bool user;         /* True: access by user, false: access by kernel. */
-	void *fault_addr;  /* Fault address. */
+page_fault(struct intr_frame *f)
+{
+	bool not_present; /* True: not-present page, false: writing r/o page. */
+	bool write;		  /* True: access was write, false: access was read. */
+	bool user;		  /* True: access by user, false: access by kernel. */
+	void *fault_addr; /* Fault address. */
 
 	/* Obtain faulting address, the virtual address that was
 	   accessed to cause the fault.  It may point to code or to
 	   data.  It is not necessarily the address of the instruction
 	   that caused the fault (that's f->rip). */
-
-	fault_addr = (void *) rcr2();
+	/* CR2 레지스터에서 에러가 난 가상 주소를 가져옴 */
+	fault_addr = (void *)rcr2();
 
 	/* Turn interrupts back on (they were only off so that we could
 	   be assured of reading CR2 before it changed). */
-	intr_enable ();
-
+	/* CR2를 읽었으니 이제 인터럽트를 다시 켜도 안전함 */
+	intr_enable();
 
 	/* Determine cause. */
-	not_present = (f->error_code & PF_P) == 0;
-	write = (f->error_code & PF_W) != 0;
-	user = (f->error_code & PF_U) != 0;
+
+	not_present = (f->error_code & PF_P) == 0; // 페이지가 없어서
+	write = (f->error_code & PF_W) != 0;	   // 쓰기 작업
+	user = (f->error_code & PF_U) != 0;		   // 유저 모드
+
+	if (user || is_kernel_vaddr(fault_addr) || not_present)
+	{
+		thread_current()->exit_status = -1;
+		thread_exit();
+	}
 
 #ifdef VM
 	/* For project 3 and later. */
-	if (vm_try_handle_fault (f, fault_addr, user, write, not_present))
+	if (vm_try_handle_fault(f, fault_addr, user, write, not_present))
 		return;
 #endif
 
@@ -150,11 +174,10 @@ page_fault (struct intr_frame *f) {
 	page_fault_cnt++;
 
 	/* If the fault is true fault, show info and exit. */
-	printf ("Page fault at %p: %s error %s page in %s context.\n",
-			fault_addr,
-			not_present ? "not present" : "rights violation",
-			write ? "writing" : "reading",
-			user ? "user" : "kernel");
-	kill (f);
+	printf("Page fault at %p: %s error %s page in %s context.\n",
+		   fault_addr,
+		   not_present ? "not present" : "rights violation",
+		   write ? "writing" : "reading",
+		   user ? "user" : "kernel");
+	kill(f);
 }
-
